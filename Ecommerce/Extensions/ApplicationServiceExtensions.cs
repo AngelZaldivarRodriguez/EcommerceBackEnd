78aabd6f1e0bd044c5d5d525506e58c810c2c43d
AngelZaldivarRodriguez <angel.zaldivar.dev@gmail.com>
2024-04-03 11:46:06 -0500

    Basket Controller and Redis

--- Ecommerce/Extensions/ApplicationServiceExtensions.cs
+++ Ecommerce/Extensions/ApplicationServiceExtensions.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Infrastucture.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 namespace Ecommerce.Extensions
 {
@@ -19,6 +20,12 @@ namespace Ecommerce.Extensions
             {
                 opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
             });
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
+            });
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
